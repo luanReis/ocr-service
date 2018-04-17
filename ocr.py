dbd486bb8c2064e0442e34d30ca1f9c96059b99a
Luan Reis <luanReis@users.noreply.github.com>
2018-04-17 00:12:05 -0300

    Covert image to grayscale before processing

--- ocr.py
+++ ocr.py
@@ -2,10 +2,22 @@ from PIL import Image
 
 import pytesseract
 import argparse
+import cv2
+import os
 
 
-def extract_text(filename):
-    return pytesseract.image_to_string(Image.open(filename))
+def generate_grayscale_image(image_path):
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
+    return filename
+
+def cleanup(temp_file_path):
+    os.remove(temp_file_path)
+
+def extract_text(image_path):
+    return pytesseract.image_to_string(Image.open(image_path))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -17,4 +29,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    print(extract_text(args.image))
+    image_path = args.image
+
+    grayscale_image = generate_grayscale_image(image_path)
+    print(extract_text(grayscale_image))
+    cleanup(grayscale_image)
