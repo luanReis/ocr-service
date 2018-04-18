0afc5f47223154dd1db8c3ba8d6f982966951718
Luan Reis <luanReis@users.noreply.github.com>
2018-04-17 23:39:49 -0300

    Extract function to deal with temp file

--- ocr.py
+++ ocr.py
@@ -8,13 +8,16 @@ import os
 
 def generate_grayscale_image(image_path):
     image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return grayscale_image
+
+def preprocess_image(image):
     filename = "{}.png".format(os.getpid())
-    cv2.imwrite(filename, gray)
+    cv2.imwrite(filename, image)
     return filename
 
-def cleanup(temp_file_path):
-    os.remove(temp_file_path)
+def cleanup(preprocessed_image):
+    os.remove(preprocessed_image)
 
 def extract_text(image_path):
     return pytesseract.image_to_string(Image.open(image_path))
@@ -32,5 +35,8 @@ if __name__ == "__main__":
     image_path = args.image
 
     grayscale_image = generate_grayscale_image(image_path)
-    print(extract_text(grayscale_image))
-    cleanup(grayscale_image)
+    preprocessed_image = preprocess_image(grayscale_image)
+
+    print(extract_text(preprocessed_image))
+
+    cleanup(preprocessed_image)
