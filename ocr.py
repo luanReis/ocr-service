f69013b675e9b9d306a35376ca62e0581c0c328b
Luan Reis <luanReis@users.noreply.github.com>
2018-04-18 00:06:49 -0300

    Add thresholding and blurring preprocess options

--- ocr.py
+++ ocr.py
@@ -6,21 +6,36 @@ import cv2
 import os
 
 
-def generate_grayscale_image(image_path):
-    image = cv2.imread(image_path)
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def _apply_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def _apply_thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+def _apply_blurring(image):
+    return cv2.medianBlur(image, 3)
+
+def preprocess(image, type):
+    grayscale_image = _apply_grayscale(image)
+
+    if type == "thresh": return _apply_thresholding(grayscale_image)
+    elif type == "blur": return _apply_blurring(grayscale_image)
+
     return grayscale_image
 
-def preprocess_image(image):
+def read_from_disk(filename):
+    return cv2.imread(filename)
+
+def save_to_disk(image):
     filename = "{}.png".format(os.getpid())
     cv2.imwrite(filename, image)
     return filename
 
-def cleanup(preprocessed_image):
-    os.remove(preprocessed_image)
+def remove_from_disk(filename):
+    os.remove(filename)
 
-def extract_text(image_path):
-    return pytesseract.image_to_string(Image.open(image_path))
+def extract_text(filename):
+    return pytesseract.image_to_string(Image.open(filename))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -30,13 +45,20 @@ if __name__ == "__main__":
         required=True,
         help="path to input image to be OCR'd")
 
+    parser.add_argument(
+        "-p",
+        "--preprocess",
+        type=str,
+        default="thresh",
+        help="type of preprocessing to be done")
+
     args = parser.parse_args()
 
-    image_path = args.image
+    image = read_from_disk(args.image)
+    preprocessed_image = preprocess(image, args.preprocess)
 
-    grayscale_image = generate_grayscale_image(image_path)
-    preprocessed_image = preprocess_image(grayscale_image)
+    temp_file = save_to_disk(preprocessed_image)
 
-    print(extract_text(preprocessed_image))
+    print(extract_text(temp_file))
 
-    cleanup(preprocessed_image)
+    remove_from_disk(temp_file)
